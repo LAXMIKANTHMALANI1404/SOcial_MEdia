ae123b22f6888ff6ef728414a368ad331a3d625d
LAXMIKANTHMALANI1404 <86907816+LAXMIKANTHMALANI1404@users.noreply.github.com>
2022-12-15 14:03:49 +0530

    postroutes created

--- server/index.js
+++ server/index.js
@@ -2,3 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();// creating object of type expess
+// app.get('/',(req,res)=>{res.send("CONNECTED")});
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb",extended:true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://kushal:kushal@cluster0.t0a8s8h.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+// mongoose.set('useFindAndModify',false);
