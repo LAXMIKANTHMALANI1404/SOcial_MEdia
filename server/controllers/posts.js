81e9fa2c3b5221c6a05f41577a9fd8e2fc37dc1e
LAXMIKANTHMALANI1404 <86907816+LAXMIKANTHMALANI1404@users.noreply.github.com>
2022-12-15 15:55:17 +0530

    functions for creating post and fetching

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,24 @@
-export const getPosts=(req,res)=>{
-    res.send("This works");
+import postMessage from "../models/postMessage.js";
+
+export const getPosts=async (req,res)=>{
+   try{
+    const postMessages=await postMessage.find();
+    res.status(200).json(postMessages);
+   }
+   catch(error){
+    res.status(404).json({message:error.message}); 
+   }
+}
+
+export const postCreate=async (req,res)=>{
+ const msg=req.body;
+ const newPost=new postMessage(msg);
+ try{
+    await newPost.save();
+    res.status(201);
+
+ }
+ catch(error){
+    res.status(409).json({message:error.message});
+ }
 }
