275982c5292cb8cbb091bfb312566b0dd7db313e
LAXMIKANTHMALANI1404 <86907816+LAXMIKANTHMALANI1404@users.noreply.github.com>
2023-01-09 15:34:29 +0530

    FULL CRED APPLICATION

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ dotenv.config();
 // const CONNECTION_URL = 'mongodb+srv://kushal:kushal@cluster0.t0a8s8h.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT||5000;
 
+
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
